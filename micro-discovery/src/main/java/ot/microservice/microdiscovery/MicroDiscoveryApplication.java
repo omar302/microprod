2e6b1ad350f79480b5d86b0950fd728e136980e3
Omar Tariq <omar3025@yahoo.com>
2022-01-19 16:45:42 +1100

    Add micro-discovery dir v0.5

---
+++ micro-discovery/src/main/java/ot/microservice/microdiscovery/MicroDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package ot.microservice.microdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroDiscoveryApplication.class, args);
+	}
+
+}
---
+++ micro-discovery/src/test/java/ot/microservice/microdiscovery/MicroDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ot.microservice.microdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
